4fb088efd9ad09292d6eb62b2f4b014e25e7bda5
adaxi <dev@adaxisoft.be>
2021-12-22 12:31:42 +0100

    Adapt extension to comply with 'Only use init() for initialization'

--- extension.js
+++ extension.js
@@ -1,134 +1,146 @@
+/* extension.js
+ *
+ * BSD 2-Clause License
+ * 
+ * Copyright (c) 2014, 2018, Andreas Fuchs
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/* exported init */
+
+const GETTEXT_DOMAIN = "audio-output-switcher@anduchs";
+
+const { GObject, St } = imports.gi;
+
+const Gettext = imports.gettext.domain(GETTEXT_DOMAIN);
+const _ = Gettext.gettext;
+
 const ExtensionUtils = imports.misc.extensionUtils;
-const Meta = imports.gi.Meta;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
-const Shell = imports.gi.Shell;
-const GObject = imports.gi.GObject;
-
-const Me = ExtensionUtils.getCurrentExtension();
-const Utils = Me.imports.utils;
 
 const AudioOutputSubMenu = GObject.registerClass(
-	class AudioOutputSubMenu extends PopupMenu.PopupSubMenuMenuItem {
-	_init() {
-		super._init("Audio Output: Connecting...", true);
-		this._control = Main.panel.statusArea.aggregateMenu._volume._control;
+  class AudioOutputSubMenu extends PopupMenu.PopupSubMenuMenuItem {
+    _init() {
+      super._init("Audio Output: Connecting...", true);
+      this._control = Main.panel.statusArea.aggregateMenu._volume._control;
 
-		this._controlSignal = this._control.connect('default-sink-changed', () => {
-			if (this._updateDefaultSink) {
-				this._updateDefaultSink();
-			}
-		});
-		if (this._updateDefaultSink) {
-			this._updateDefaultSink();
-		}
-		this.menu.connect('open-state-changed', (menu, isOpen) => {
-			if (isOpen)
-				this._updateSinkList();
-		});
-		//Unless there is at least one item here, no 'open' will be emitted...
-		let item = new PopupMenu.PopupMenuItem('Connecting...');
-		this.menu.addMenuItem(item);
-	}
+      this._controlSignal = this._control.connect(
+        "default-sink-changed",
+        () => {
+          if (this._updateDefaultSink) {
+            this._updateDefaultSink();
+          }
+        }
+      );
+      if (this._updateDefaultSink) {
+        this._updateDefaultSink();
+      }
+      this.menu.connect("open-state-changed", (menu, isOpen) => {
+        if (isOpen) {
+          this._updateSinkList();
+        }
+      });
 
-	_updateDefaultSink() {
-		let defsink = this._control.get_default_sink();
-		//Unfortunately, Gvc neglects some pulse-devices, such as all "Monitor of ..."
-		if (!defsink)
-			this.label.set_text("Other");
-		else
-			this.label.set_text(defsink.get_description());
-	}
+      //Unless there is at least one item here, no 'open' will be emitted...
+      let item = new PopupMenu.PopupMenuItem("Connecting...");
+      this.menu.addMenuItem(item);
+    }
 
-	_updateSinkList() {
-		this.menu.removeAll();
+    _updateDefaultSink() {
+      let defaultSink = this._control.get_default_sink();
+      //Unfortunately, Gvc neglects some pulse-devices, such as all "Monitor of ..."
+      if (!defaultSink) {
+        this.label.set_text("Other");
+      } else {
+        this.label.set_text(defaultSink.get_description());
+      }
+    }
 
-		let defsink = this._control.get_default_sink();
-		let sinklist = this._control.get_sinks();
-		let control = this._control;
+    _updateSinkList() {
+      this.menu.removeAll();
 
-		for (let i=0; i<sinklist.length; i++) {
-			let sink = sinklist[i];
-			if (sink === defsink)
-				continue;
-			let item = new PopupMenu.PopupMenuItem(sink.get_description());
-			item.connect('activate', () => {
-				control.set_default_sink(sink);
-			});
-			this.menu.addMenuItem(item);
-		}
-		if (sinklist.length == 0 ||
-			(sinklist.length == 1 && sinklist[0] === defsink)) {
-			let item = new PopupMenu.PopupMenuItem("No more Devices");
-			this.menu.addMenuItem(item);
-		}
-	}
+      let defaultSink = this._control.get_default_sink();
+      let sinkList = this._control.get_sinks();
+      let control = this._control;
 
-	destroy() {
-		this._control.disconnect(this._controlSignal);
-		super.destroy();
-	}
-});
+      for (let i = 0; i < sinkList.length; i++) {
+        let sink = sinkList[i];
+        if (sink === defaultSink) continue;
+        let item = new PopupMenu.PopupMenuItem(sink.get_description());
+        item.connect("activate", () => {
+          control.set_default_sink(sink);
+        });
+        this.menu.addMenuItem(item);
+      }
+      if (
+        sinkList.length == 0 ||
+        (sinkList.length == 1 && sinkList[0] === defaultSink)
+      ) {
+        let item = new PopupMenu.PopupMenuItem("No more Devices");
+        this.menu.addMenuItem(item);
+      }
+    }
 
-let sinkIndex = 0;
-let settings = null;
-let audioOutputSubMenu = null;
+    destroy() {
+      this._control.disconnect(this._controlSignal);
+      super.destroy();
+    }
+  }
+);
 
-function init () {
-	sinkIndex = 0;
-	settings = Utils.getSettings();
+class Extension {
+  constructor(uuid) {
+    this._uuid = uuid;
+
+    ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+  }
+
+  enable() {
+    this._menu = new AudioOutputSubMenu();
+
+    // Try to add the output-switcher right below the output slider...
+    const volumeMenu = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
+    let items = volumeMenu._getMenuItems();
+    let i = 0;
+    while (i < items.length) {
+      if (items[i] === volumeMenu._output.item) {
+        break;
+      }
+      i++;
+    }
+    volumeMenu.addMenuItem(this._menu, i + 1);
+  }
+
+  disable() {
+    this._menu.destroy();
+    this._menu = null;
+  }
 }
 
-function enable () {
-	if (audioOutputSubMenu) {
-		return;
-	}
-	audioOutputSubMenu = new AudioOutputSubMenu();
-
-	//Try to add the output-switcher right below the output slider...
-	let volMen = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
-	let items = volMen._getMenuItems();
-	let i = 0;
-	while (i < items.length)
-		if (items[i] === volMen._output.item)
-			break;
-		else
-			i++;
-	volMen.addMenuItem(audioOutputSubMenu, i+1);
-
-	//Add keyboard shortcut for fast switching
-
-	let keyBindingMode = null;
-	if (Shell.ActionMode) {
-		//KeyBindingMode was renamed to ActionMode in Gnome 3.15.3
-		keyBindingMode = Shell.ActionMode.ALL;
-	} else {
-		keyBindingMode = Shell.KeyBindingMode.ALL;
-	}
-
-	// Main.wm.addKeybinding("switch-next-audio-output",
-	// 	settings,
-	// 	Meta.KeyBindingFlags.NONE,
-	// 	keyBindingMode,
-	// 	function(display, screen, window, binding) {
-
-	// 		let control = Main.panel.statusArea.aggregateMenu._volume._control;
-	// 		let sinklist = control.get_sinks();
-
-	// 		if (sinklist.length === 0) {
-	// 			return;
-	// 		}
-	// 		sinkIndex++
-	// 		if (sinkIndex >= sinklist.length) {
-	// 			sinkIndex = 0
-	// 		}
-	// 		let sink = sinklist[sinkIndex];
-	// 		control.set_default_sink(sink);
-	// 	}
-	// );
-}
-
-function disable () {
-	audioOutputSubMenu.destroy();
-	audioOutputSubMenu = null;
+function init(meta) {
+  return new Extension(meta.uuid);
 }
